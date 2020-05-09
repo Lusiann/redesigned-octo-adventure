f12da9c87a237575da04b2a3b67a053687966830
Ludovico <ludovico@ludovico-optiplex7010fedora.station>
2020-05-09 13:00:39 +0200

    terzo atto

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@
 function navSlide() {
     const burger = document.querySelector('.burger')
     const nav = document.querySelector('.nav-links')
+    let isMouseDown = false;
     burger.addEventListener('click', () => {
+
         nav.classList.toggle('nav-active')
     })
+    
 }
 
 
@@ -20,4 +23,30 @@ new Glide('.glide', {
 
 new Cocoen(document.querySelector('.cocoen'));
 
+
+
+function continueReading() {
+    const dots = document.getElementById('dots')
+    const moreText = document.getElementById('more')
+    const btnText = document.getElementById('myBtn')
+
+    btnText.addEventListener('click', () => {
+
+        
+        if (dots.style.display === 'none') {
+            dots.style.display = 'inline'
+            btnText.innerHTML = 'Leggi di più'
+            moreText.style.display = 'none'
+        } else {
+            dots.style.display = 'none'
+            btnText.innerHTML = 'Minimizza'
+            moreText.style.display = 'inline'
+        }
+
+    })
+
+    
+}
+
+continueReading()
 
